5c29385969a3e8241396e0dc7e83da144c5dc834
Jim Drewes <jim@jimdrewes.com>
2015-12-16 21:12:59 -0600

    Initial import.

---
+++ src/TestClassLibrary/Class1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public int TestInt { get; set; }
+        public string MyString { get; set; }
+
+
+
+    }
+}
